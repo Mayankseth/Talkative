0c92eaea3fd6acaf6c3a453ff98fa5f9c0a013fd
Mayankseth <sethmayank33@gmail.com>
2024-07-12 23:31:46 +0530

    jwt

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const chats = require('./data/data');
+const connectDB = require('./config/db');
+const colors= require('colors')
+const userRoutes = require('./routes/userRoutes')
 
-const app= express();
 dotenv.config();
+connectDB();
+const app= express();
+
+app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("API is running good success");
 });
 
-app.get("/api/chat",(req,res)=>{
-    res.send(chats);
-})
+app.use('/api/user',userRoutes)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, console.log(`Server listening on port ${PORT}`.yellow.bold));
