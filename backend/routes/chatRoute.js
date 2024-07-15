2ae1e8a9951107b14ecb2d4b8d35b0e3cb0dc951
Mayankseth <sethmayank33@gmail.com>
2024-07-16 00:08:08 +0530

    group

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -1,13 +1,13 @@
-const { accessChat } = require("../controllers/chatController");
+const { accessChat, fetchChats, createGroupChat } = require("../controllers/chatController");
 const {protect} = require ("../middleware/authMiddleware");
 
-const express = requuire("express");
+const express = require("express");
 
 const router = express.Router();
 
 router.route("/").post(protect,accessChat);
-// router.route("/").get(protect,fetchChats);
-// router.route("/group").post(protect,createGroupChat);
+router.route("/").get(protect,fetchChats);
+router.route("/group").post(protect,createGroupChat);
 // router.route("/rename").put(protect,renameGroup);
 // router.route("/groupremove").put(protect,removeFromGroup);
 // router.route("/groupadd").put(protect,addToGroup);
