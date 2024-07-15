362329ecc2992f35f485da5d94a0a04e10507318
Mayankseth <sethmayank33@gmail.com>
2024-07-15 17:33:06 +0530

    newchat

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const chats = require('./data/data');
 const connectDB = require('./config/db');
 const colors= require('colors')
 const userRoutes = require('./routes/userRoutes');
+const chatRoute = require("./routes/chatRoute")
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.get("/",(req,res)=>{
 });
 
 app.use('/api/user',userRoutes)
+app.use('/api/chat',chatRoute)
 
 app.use(notFound)
 app.use(errorHandler)
